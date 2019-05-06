8b02b3d6ea63060fdddd7313a8e65732d079cf43
Papytuc1 <kevinsanmartino@gmail.com>
2019-05-06 10:13:13 +0200

    correction sur le datalist

--- ExoLaurent/exoSemaine-29-03-mai/gestionUtilisateurs/js/main.js
+++ ExoLaurent/exoSemaine-29-03-mai/gestionUtilisateurs/js/main.js
@@ -1,42 +1,44 @@
-var email = document.querySelector("#email");
+(function(){
+    var email = document.querySelector("#email");
 
-email.addEventListener("keyup", function (event) {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Email non valide");
-    } else {
-        email.setCustomValidity("");
-    }
-});
-
-function request(datas) {
-    fetch(datas.url || "").then(response => {
-        if (response.ok) {
-            return response.json();
-        };
-    }).then(result => {
-        datas.complete && datas.complete(result);
-    });
-};
-let villes = {};
-
-function ville(name) {
-    request({
-        url: `https://geo.api.gouv.fr/communes?nom=${name}&fields=nom&boost=${name},codesPostaux&format=json&geometry=centre`,
-        complete: (e) => {
-            villes = e;
+    email.addEventListener("keyup", function (event) {
+        if (email.validity.typeMismatch) {
+            email.setCustomValidity("Email non valide");
+        } else {
+            email.setCustomValidity("");
         }
-    })
-}
-let inputVille = document.querySelector("[name='ville']");
-let dataList = document.querySelector("datalist");
-inputVille.addEventListener("keyup", function (e) {
-    if (this.value.length > 3) {
-        dataList.innerHTML = "";
-        ville(this.value);
-            villes.forEach(ville => {
-                let option = document.createElement("option");
-                option.textContent = ville.nom;
-                dataList.appendChild(option);
-            });
+    });
+    
+    function request(datas) {
+        fetch(datas.url || "").then(response => {
+            if (response.ok) {
+                return response.json();
+            };
+        }).then(result => {
+            datas.complete && datas.complete(result);
+        });
+    };
+    let villes = {};
+    
+    function ville(name) {
+        request({
+            url: `https://geo.api.gouv.fr/communes?nom=${name}&fields=nom&boost=${name}&format=json&geometry=centre`,
+            complete: (e) => {
+                villes = e.slice(0,5);
+                dataList.innerHTML = "";
+                    villes.forEach(ville => {
+                        let option = document.createElement("option");
+                        option.textContent = ville.nom;
+                        dataList.appendChild(option);
+                    });
+            }
+        })
     }
-})
+    let inputVille = document.querySelector("[name='ville']");
+    let dataList = document.querySelector("datalist");
+    inputVille.addEventListener("keyup", function () {
+        if (this.value.length >= 2) {
+            ville(this.value);
+        }
+    });
+})()
